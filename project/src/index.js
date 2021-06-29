be6e37890a8b53af634c4423e4e9b888a6d368af
Ksenia Bondarchuk <kseny.bondarchuk@gmail.com>
2021-06-29 04:52:22 +0300

    implement first components

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const filmsList = [
+  'Fantastic Beasts: The Crimes of Grindelwald',
+  'Bohemian Rhapsody',
+  'Macbeth',
+  'Aviator',
+  'We need to talk about Kevin',
+  'What We Do in the Shadows',
+  'Revenant',
+  'Johnny English',
+  'Shutter Island',
+  'Pulp Fiction',
+  'No Country for Old Men',
+  'Snatch',
+  'Moonrise Kingdom',
+  'Seven Years in Tibet',
+  'Midnight Special',
+  'War of the Worlds',
+  'Dardjeeling Limited',
+  'Orlando',
+  'Mindhunter',
+  'Midnight Special',
+];
+
+const headerFilmTitle = 'The Grand Budapest Hotel';
+const headerFilmGenre = 'Drama';
+const headerFilmYear = '2014';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App filmsList={filmsList}
+      headerFilmTitle={headerFilmTitle}
+      headerFilmGenre={headerFilmGenre}
+      headerFilmYear={headerFilmYear}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
